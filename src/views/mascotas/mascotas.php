d72a4eada9cbbf0cf596c827d50b9584285ad19a
yessica <alefres234@gmail.com>
2023-09-14 14:44:40 -0600

    Contenido de la pagina mascota

--- src/views/mascotas/mascotas.php
+++ src/views/mascotas/mascotas.php
@@ -47,13 +47,13 @@
             <!-- Contenido Principal -->
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2 text-dark">Administracion de mascotas</h1>
+                    <h1 class="h2 text-dark">Administración Mascotas</h1>
                 </div>
                 <!-- Aquí va el contenido de la página -->
                 <div class="container">
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="bi bi-plus"></i> Nuevo mascota
+                        <i class="bi bi-plus">Nueva Mascota</i>
                     </button>
 
                     <!-- Modal -->
